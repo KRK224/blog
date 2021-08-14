93c42c536ba687d22399bcd63d1e73c9022c8477
KRyun <hihas93@naver.com>
2021-08-14 23:27:43 +0900

    koa router module

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,7 +1,34 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+/* 
+router.get('/', ctx=>{
+  ctx.body = '홈';
+});
+
+router.get('/about/:name?', ctx=>{
+  const {name} = ctx.params;
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', ctx=>{
+  const {id} = ctx.query;
+  ctx.body = id ? `포스트 #${id}`: '포스트 아이디가 없습니다.';
+}); 
+*/
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+/* 
 app.use(async (ctx, next)=>{
   console.log(ctx.url);
   console.log(1);
@@ -21,7 +48,7 @@ app.use((ctx, next)=>{
 
 app.use(ctx => {
   ctx.body = 'hello world';
-});
+}); */
 
 app.listen(4000, () =>{
   console.log('Listening to port 4000');
