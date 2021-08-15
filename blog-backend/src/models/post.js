6ff346ba9ae125404d3c9971bdad646c61e62b73
KRyun <hihas93@naver.com>
2021-08-15 20:49:33 +0900

    Schema & model 생성

---
+++ blog-backend/src/models/post.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+const PostSchema = new Schema({
+  title: String,
+  body: String,
+  tags: [String],
+  publishedDate: {
+    type: Date,
+    default: Date.now, // 현재 날짜를 기본 값으로 지정.
+  }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
+
